8b791eb0de3dbf5d5e96c48b975fe90c0a268de6
Elvio Galeano <elvio.galeano827@gmail.com>
2024-04-12 15:38:10 -0300

    sidebar

---
+++ src/constants/sidebarLinks.js
@@ -0,0 +1,24 @@
+import { BsListTask } from "react-icons/bs";
+
+export const sidebarLinks = [
+    {
+        // icon: <BsListTask />,
+        route: "/tasks",
+        label: "All Tasks"
+    },
+    {
+        // icon: <BsListTask />,
+        route: "/tasks",
+        label: "Pending Tasks"
+    },
+    {
+        // icon: <BsListTask />,
+        route: "/tasks",
+        label: "Accept Tasks"
+    },
+    {
+        // icon: <BsListTask />,
+        route: "/tasks",
+        label: "Reject Tasks"
+    },
+]
--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { sidebarLinks } from '../constants/sidebarLinks'
+import { Link } from 'react-router-dom'
 
 export default function TasksPage() {
   return (
-    <div>TasksPage</div>
+    <div className='bg-zinc-900'>
+      <div className='bg-zinc-600 max-w-xs flex flex-col items-center h-screen justify-around'>
+        <h1>My Tasks</h1>
+        <div className='flex flex-col gap-4'>
+          {sidebarLinks.map(item => (
+            <Link to={item.route} key={item.label}>{item.label}</Link>
+          ))}
+        </div>
+        <h1>Log out</h1>
+      </div>
+    </div>
   )
 }
